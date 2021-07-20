2b677425975bac4278a5d11716d76c253399ec72
unknown <20ishita@gmail.com>
2021-07-20 18:56:17 +0530

    add navbar and hero carousel

---
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import HeroSlider from "react-slick";
+
+//mport css file
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HeroCarousel = () => {
+    const settingsLG = {
+        arrows: true,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: "300px",
+        slidesToShow: 1,
+        infinite: true,
+        slidesToScroll: 1,
+    }
+    const settings = {
+        arrows: true,
+        centerPadding: "200px",
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+    const images = [
+        "https://images.unsplash.com/photo-1606857090627-27ca46667290?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1947&q=80",
+        "https://images.unsplash.com/photo-1546948630-1149ea60dc86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1620398762817-ff3885718863?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fHNwaXRpfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1585910165842-6bcd6ee68f6e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3Vuc2V0c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1549880181-56a44cf4a9a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1617454298947-9fcc63553d41?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHN1bnNldHN8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdW50YWlufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        "https://images.unsplash.com/photo-1599600197870-4bfaf17d5f42?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1526925528837-813a7961f5c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1552083375-1447ce886485?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fG5hdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+    ];
+
+    return (
+        <>
+            <div className="lg:hidden">
+                <HeroSlider {...settings}>
+                    {images.map((image) => (
+                        <div className="w-full h-56 md:h-80 py-3">
+                            <img src={image} alt="testing" className="w-full h-full" />
+                        </div>
+                    ))}
+                </HeroSlider>
+            </div>
+            <div className="hidden lg:block">
+            <HeroSlider {...settingsLG}>
+                    {images.map((image) => (
+                        <div className="w-full h-96 px-2 py-3">
+                            <img src={image} alt="testing" className="w-full h-full" />
+                        </div>
+                    ))}
+                </HeroSlider>
+            </div>
+        </>);
+};
+
+export default HeroCarousel;
