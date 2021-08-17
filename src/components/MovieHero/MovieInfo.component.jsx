8f2def029054403a0d2be9180164edbe4046c1c2
unknown <20ishita@gmail.com>
2021-08-17 20:56:09 +0530

    add payment gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,12 +1,26 @@
-import React,{useContext} from "react";
+import React,{useContext,useState} from "react";
 
 import { MovieContext } from "../../context/movie.context";
 
+import PaymentModal from "../PaymentModel/payment.component";
+
 const MovieInfo = () => {
+    const [isOpen,setIsOpen] =useState(false);
+    const [price,setPrice]=useState(0)
     const {movie}=useContext(MovieContext);
-    const genres=movie.genres?.map(({name})=>name).join(", ");
+    const genres=movie.genres?.map(({name})=>name).join(", ");  
+
+    const rentMovies=()=>{
+        setIsOpen(true);
+        setPrice(149);
+    }
+    const buyMovies=()=>{
+        setIsOpen(true);
+        setPrice(689);
+    }
     return (
         <>
+        <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
             <div className="flex flex-col gap-3 lg:gap-8">
                 <div className="flex items-center gap-3 md:px-4">
                     <div className="w-40 h-8">
@@ -21,8 +35,8 @@ const MovieInfo = () => {
                         <h4>{(movie.runtime/60).toFixed(2)}h &bull; {genres} &bull; 13+ &bull; 24 Mar,2021 </h4>
                     </div>
                     <div className="flex items-center gap-3 md:w-screen px-4 lg:w-full">
-                        <button className="bg-red-600 w-full py-5 text-white font-semibold rounded-lg">Rent ₹149</button>
-                        <button className="bg-red-600 w-full py-5 text-white font-semibold rounded-lg">Buy ₹689</button>
+                        <button onClick={rentMovies} className="bg-red-600 w-full py-5 text-white font-semibold rounded-lg">Rent ₹149</button>
+                        <button onClick={buyMovies} className="bg-red-600 w-full py-5 text-white font-semibold rounded-lg">Buy ₹689</button>
                     </div>
                 </div>
             </div>
